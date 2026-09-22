06f71be59e379a361aadb7a238f1816ee4074151
Ange-Raphael <raphaellouanne@gmail.com>
2026-09-23 00:24:28 +0200

    ✨ feat: init nix config :)

---
+++ nix/home/default.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+
+  home.username = "arn";
+  home.homeDirectory = "/Users/arn";
+
+  home.stateVersion = "26.05";
+
+  home.packages = [
+  ];
+
+  home.file = {
+  };
+
+  home.sessionVariables = {
+  };
+
+  programs.home-manager.enable = true;
+}
