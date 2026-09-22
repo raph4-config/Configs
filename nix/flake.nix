14010e7197a105b801652f45635364c7b5422cad
⚔️ Ange-Raphael ⚔️ <48210678+raph4-config@users.noreply.github.com>
2026-09-23 00:28:50 +0200

    Feat/nix config (#41)
    * ✨ feat: init nix config :)
    
    * ✨ feat: init nix config :)

---
+++ nix/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Home Manager configuration of Raph4";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."arn" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home/default.nix ];
+      };
+    };
+}
