8af75e8610cf3eb1ff149dc7c52f3ce686626321
Ange-Raphael <raphaellouanne@gmail.com>
2026-09-23 02:13:18 +0200

    ✨ feat: add module cli (tree pkgs)

--- nix/home/default.nix
+++ nix/home/default.nix
@@ -1,20 +1,19 @@
 { config, pkgs, ... }:
 {
 
+  imports = [ ../modules/cli.nix ];
+
   home.username = "arn";
+
   home.homeDirectory = "/Users/arn";
 
   home.stateVersion = "26.05";
 
-  home.packages = [
-    pkgs.tree
-  ];
+  home.packages = [];
 
-  home.file = {
-  };
+  home.file = {};
 
-  home.sessionVariables = {
-  };
+  home.sessionVariables = {};
 
   programs.home-manager.enable = true;
 }
